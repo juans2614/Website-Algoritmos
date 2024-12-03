62539ecfc6d442932f2dc7e6e24ba5df7dda4e15
Juan Hernandez <sebbbbas2601@gmail.com>
2024-12-03 07:44:26 -0500

    Changes 03/12

--- algoritmos.js
+++ algoritmos.js
@@ -1,106 +1,45 @@
-//1.Algoritmo que realiza la suma entre 2 valores integrados por el usuario
-function suma(){
-    //declaracion de las variables necesarias para este algoritmo
-    let A;
-    let B;
-    let suma;
+//Saludar
 
-    //solicito los valores al usuario
-    A = parseInt(prompt(' Ingrese el primer valor a sumar'));
-    B = parseInt(prompt(' Ingrese el segundo valor a sumar'));
 
-    //realizar operacion
-    S = A + B;
-
-    //Mostrar el resultado
-    console.log(" El resultado de la suma es:" + S);
-    alert(" El resultado de la suma es:" + S);
+function Saludar(){
+    alert("Hola Usuario");
 }
-
-
-// 2. Algoritmo que determina la suma, resta, multiplicacion y division entre 2 valores ingresados por el usuario
 
 function opBasicas(){
-    //declaracion de las variables necesarias para este algoritmo
-    let A;
-    let B;
-    let C;
-    let D;
-    let E;
-    let F;
-    let G;
-    let H;
-    let suma;
-    let resta;
-    let multiplicacion;
-    let division;
-    
-    //solicito los valores al usuario
+    // Declaro las variables necearias para la ejecucuión
+   let A,B,S,R,M,D = 0;
+    // Nnotifico el usuario que realiza este algoritmo
+   alert("Este algoritmo realiza una suma, resta, multiplicación y divison entre dos valores ingresados")
+    // Capturar los datos de entrada o inputs
+   A = parseInt(prompt("Por favor ingrese el primer valor"));
+   B = parseInt(prompt("Por favor ingrese el segundo valor"));
+    // Realizar el proceso
+   S = A + B;
+   R = A - B;
+   M = A * B;
+   D = A / B;
 
-    A = parseInt(prompt(' Ingrese el primer valor para sumar'));
-    B = parseInt(prompt(' Ingrese el segundo valor a sumar'));
-    C = parseInt(prompt(' Ingrese el primer valor para restar'));
-    D = parseInt(prompt(' Ingrese el segundo valor a restar'));
-    E = parseInt(prompt(' Ingrese el primer valor para multiplicar'));
-    F = parseInt(prompt(' Ingrese el segundo valor a multiplicar'));
-    G = parseInt(prompt(' Ingrese el primer valor para dividir'));
-    H = parseInt(prompt(' Ingrese el segundo valor a dividir'));
-
-    //realizar operacion
-
-    suma = A + B;
-    resta = C - D;
-    multiplicacion = E * F;
-    division = G / H;
-
-    //Mostrar el resultado
-
-    console.log(" El resultado de la suma es:" + suma);
-    alert(" El resultado de la suma es:" + suma);
-
-    console.log(" El resultado de la resta es:" + resta);
-    alert(" El resultado de la suma es:" + resta);
-
-    console.log(" El resultado de la multiplicacion es:" + multiplicacion);
-    alert(" El resultado de la multiplicacion es:" + multiplicacion);
-
-    console.log(" El resultado de la division es:" + division);
-    alert(" El resultado de division es:" + division);
+   // Dar el resultado
+   alert("El resultado de la suma es: " + S);
+   alert("El resultado de la resta es: " + R);
+   alert("El resultado de la multiplicación es: " + M);
+   alert("El resultado de la división es: " + D);
 
 }
 
-// 3. realizar un algoritmo que determine el cuadrado de un numero ingresado por el usuario.
-    
-function camNumeracion(){
+function areaTriangulo(){
+    let B,H,A = 0;
 
-    let A;
-    let B;
+    alert("Este algoritmo calcula el área de un triangulo a partir de su base y su altura")
 
-    A = parseInt(prompt('Ingrese valor para calcularlo:'));
-    B = parseInt(prompt('Ingrese valor para calcularlo:'));
-    
+    B = parseInt(prompt("Ingrese el valor Base"))
+    H = parseInt(prompt("Ingrese el valor Altura"))
 
-    M = A * B;
+    A = (B*H)/2
 
-    console.log("El valor ingresado es:" + M);
-    alert("El valor ingresado es:" + M );
+    alert("El área de triangulo es: " + A)
 }
-// 4. realizar un algoritmo que determine el area de un triangulo apartir de la base y altura ingresadas por el usuario.
- 
-function aTriangulo(){
 
-    let base;
-    let altura;
-
-    base = parseInt(prompt('Ingrese el numero de la base'));
-    altura = parseInt(prompt('Ingrese el numero de la altura'));
-
-    M = (base * altura) /2;
-
-    console.log("El area del triangulo" + M)
-    alert ("El area del triangulo es:" + M)
-}
-// 5. realizar un algoritmo que determine kilometros los pulgadas y metros de un valor dado en centimetros.
 function conversionUnidades(){
 
     let M, P, CM, KM = 0
@@ -119,145 +58,382 @@ function conversionUnidades(){
 
 }
 
-// 6. realizar un algoritmo que pregunte al usuario a que moneda quiere convertir al peso colombiano entre dolar, euro,llenes con swich case
+// Algoritmos condicionales
 
-function conversionMoneda() {
+function servicioMilitar(){
+    let edad = 0;
+    let genero = "";
+    let hijoUnico = "";
 
-//Objeto como dato de aprtida
-let tasaCambio = {
-    USD : 0.00023,
-    EUR : 0.00024,
-    YEN : 0.035,
-}
+    alert("Algoritmo que determina si eres apto para prestar el servicio militar obligatorio");
 
-alert("Realiza la conversión de dinero en pesos Colombianos a dolares, euros y yenes");
+    edad = parseInt(prompt("Por favor ingrese su edad"));
+    genero = prompt("Por favor ingrese su género: F- femenino o M- masculino");
+    hijoUnico = prompt("Por favor ingrese SÍ o NO, de ser único");
 
-let cantidadPesos = parseFloat(prompt("Ingrese la cantidad a convertir"));
-let tasa = parseInt(prompt("Seleccione la divisa a cambiar: USD = 1, EUR = 2, YEN = 3"));
-let conversion;
-switch (tasa) {
-    case 1:
-        conversion = cantidadPesos * tasaCambio.USD;
-        alert(cantidadPesos + " Pesos Colombianos son " + conversion + "Dolares");
-        break;
-    case 2:
-        conversion = cantidadPesos * tasaCambio.EUR;
-        alert(cantidadPesos + " Pesos Colombianos son " + conversion + "Euros");
-    case 3:
-            conversion = cantidadPesos * tasaCambio.YEN;
-            alert(cantidadPesos + " Pesos Colombianos son " + conversion + "Yenes");
+    if (edad>=18 && edad<=26) {
         
-        break;
+        if (genero === "M") {
 
-    default:
-    alert("Tase de cambia no permitida");
-        break;
+            if (hijounico === "NO") {
+
+                alert("Eres apto para prestar el sevicio militar");
+
+            }else{
+                alert("No eres apto para prestar el servicio militar");
+            }
+
+        }else{
+            alert("No eres apta para prestar servicio militar")
+        }
+
+    }else{
+        alert("No es apto por su edad");
+    }
 }
 
+function mayorDosN(){
+
+    let N1,N2 = 0;
+
+    alert("Valida el mayor de dos números");
+
+    N1 = parseInt(prompt("Ingrese el primer valor"));
+    N2 = parseInt(prompt("Ingrese el segundo valor"));
+
+    if (N1 === N2) {
+        alert("No hay ningun número mayor. Ambos son iguales");
+    }else if(N1>N2){
+        alert(N1 + " es mayor que " + N2);
+    }else{
+        alert(N2 + " es ayor que" + N1);
+    }
+
 }
 
-
-
-// 7. algoritmo si un numero es par o impar 
 
 function paroImpar() {
 
-let X = 0;
+    let X = 0;
 
-alert("Determinar si un número es par o impar");
+    alert("Determinar si un número es par o impar");
 
-X = parseInt(prompt("Ingrese el número a verificar"));
+    X = parseInt(prompt("Ingrese el número a verificar"));
 
-if (X % 2 === 0){
-    alert(X + " es un número par");
-}else{
-    alert(X + " es un número impar");
+    if (X % 2 === 0){
+        alert(X + " es un número par");
+    }else{
+        alert(X + " es un número impar");
+    }
+
+
 }
 
 
+function rangoNumeros(){
+
+    alert("Imprimir en consola los números existentes entre un valor inicial y un valor final");
+
+    let inicio = parseInt(prompt("Ingrese el valor de partida"));
+    let fin = parseInt(prompt("Ingrese el valor de final"));
+
+    let contador = inicio;
+
+    while(contador <= fin) {
+        console.log(contador);
+
+        contador ++;
+    }
+
+    alert("En consola mostraremos lo número que hay entre " +inicio + " y " + fin );
+
+
+}
+
+function sumaRango(){
+    alert("Súma los valores entre un rango ingresado");
+
+    let inicio = parseInt(prompt("Ingrese el punto de partida"));
+    let fin = parseInt(prompt("Ingrese el punto de final"));
+
+    let suma = 0;
+
+    contador = inicio;
+
+    while(contador <= fin){
+        suma += contador;
+
+        contador ++;
+    }
+
+    alert("La suma de los numeros del " + inicio + " al " + fin + " es: " + suma );
+
+}
+
+function esPrimo(){
+    alert("Determina si un n umero es primo");
+
+    let numero = parseInt(prompt("Ingrese el número"));
+
+    if(numero<=1){
+        alert(numero + " no es un número primo");
+    }
+
+    for(let i = 2; i <= numero/2; i++){
+
+        if(numero % i === 0){
+            alert(numero + "No es un número primo");
+            return;
+        }
+
+    }
+
+    alert(numero + " si es un número primo");
+
+}
+
+function conversionMoneda() {
+    
+    //Objeto como dato de aprtida
+    let tasaCambio = {
+        USD : 0.00028,
+        EUR : 0.00024
+    }
+
+    alert("Realiza la conversión de dinero en pesos Colombianos a dolares y euros");
+
+    let cantidadPesos = parseFloat(prompt("Ingrese la cantidad a convertir"));
+    let tasa = parseInt(prompt("Seleccione la divisa a cambiar: USD = 1, EUR = 2"));
+    let conversion;
+    switch (tasa) {
+        case 1:
+            conversion = cantidadPesos * tasaCambio.USD;
+            alert(cantidadPesos + " Pesos Colombianos son " + conversion + "Dolares");
+            break;
+        case 2:
+            conversion = cantidadPesos * tasaCambio.EUR;
+            alert(cantidadPesos + " Pesos Colombianos son " + conversion + "Euros");
+            
+            break;
+
+        default:
+        alert("Tase de cambia no permitida");
+            break;
+    }
+
+}
+
+function calcularPromedio(){
+    alert("Este algoritmo determina el promedio de calificaciones ingresados por el usuario");
+
+    let calificaciones = [];
+
+    let cantidadNotas = parseInt(prompt("Ingrese la cantidad de calificaciones"));
+
+    for (let i = 1; i <= cantidadNotas; i++) {
+        calificaciones.push(parseFloat(prompt("Ingrese la calificacion # " + i + " :")));      
+    }
+
+    let sumaCalif = 0;
+    for(let calificacion of calificaciones){
+        sumaCalif += calificacion;
+    }
+
+    let promedio = sumaCalif/calificaciones.length;
+
+    alert("El promedio de las calificaciones es: " + promedio);
+}
+
+function consultarPeli(){
+
+    alert("Este algoritmo muestra la información de una pelicula elegida");
+
+    let peliculas = [
+        {
+            titulo : "El padrino",
+            director : "Nombre del director",
+            anio : "1972",
+            genero: "Drama"
+        },
+        {
+            titulo : "Interestellar",
+            director : "Nombre del director",
+            anio : "2014",
+            genero: "Ficción"
+        },
+        {
+            titulo : "Harry Potter",
+            director : "Nombre del director",
+            anio : "2008",
+            genero: "Ficción"
+        }
+
+    ];
+
+    let nombrePelicula = prompt("Por favor ingrese el nombre de la pelicula");
+
+    let infoPelicula = "";
+
+    for(let pelicula of peliculas){
+        
+        if(pelicula.titulo === nombrePelicula){
+            infoPelicula = "Titulo: " + pelicula.titulo + "\n" + 
+                            "Director" + pelicula.director + "\n" + 
+                            "Año:" + pelicula.anio + "\n" + 
+                            "Genero" + pelicula.genero;
+                break;
+        }
+
+    }
+
+    if(infoPelicula !== ""){
+        alert("La pelicula consultada tiene la información " + infoPelicula);
+    }
+    else{
+        alert("La pelicula no se encuentra en nuestro sistema");
+    }
+
+}
+
+// realizar un algoritmo que pregunte al usuario a que moneda quiere convertir al peso colombiano entre dolar, euro,llenes con swich case
+
+function conversionMoneda() {
+    
+    //Objeto como dato de aprtida
+    let tasaCambio = {
+        USD : 0.00023,
+        EUR : 0.00024,
+        YEN : 0.035,
+    }
+
+    alert("Realiza la conversión de dinero en pesos Colombianos a dolares, euros y yenes");
+
+    let cantidadPesos = parseFloat(prompt("Ingrese la cantidad a convertir"));
+    let tasa = parseInt(prompt("Seleccione la divisa a cambiar: USD = 1, EUR = 2, YEN = 3"));
+    let conversion;
+    switch (tasa) {
+        case 1:
+            conversion = cantidadPesos * tasaCambio.USD;
+            alert(cantidadPesos + " Pesos Colombianos son " + conversion + "Dolares");
+            break;
+        case 2:
+            conversion = cantidadPesos * tasaCambio.EUR;
+            alert(cantidadPesos + " Pesos Colombianos son " + conversion + "Euros");
+        case 3:
+                conversion = cantidadPesos * tasaCambio.YEN;
+                alert(cantidadPesos + " Pesos Colombianos son " + conversion + "Yenes");
+            
+            break;
+
+        default:
+        alert("Tase de cambia no permitida");
+            break;
+    }
+
 }
 
 
-// 8. algoritmo que determine el amyor entre 2 numeros ingresados por el usuario :)iffffff
+
+// algoritmo si un numero es par o impar 
+
+function paroImpar() {
+
+    let X = 0;
+
+    alert("Determinar si un número es par o impar");
+
+    X = parseInt(prompt("Ingrese el número a verificar"));
+
+    if (X % 2 === 0){
+        alert(X + " es un número par");
+    }else{
+        alert(X + " es un número impar");
+    }
+
+
+}
+
+
+// algoritmo que determine el amyor entre 2 numeros ingresados por el usuario :)iffffff
 
 function entreDosNum(){
 
-alert ("Este algoritmo determina el mayor entre dos numeros")
+    alert ("Este algoritmo determina el mayor entre dos numeros")
 
-let numero1 = 0
-let numero2 = 0
-
-numero1 = parseFloat(prompt("Ingrese el primer numero"));
-numero2 = parseFloat(prompt("Ingrese el segundo numero"));
-
-if (numero1 === numero2){
-    console.log("El numero es igual" + numero1 === numero2)
-    alert("El numero es igual")
-}
-
-else if (numero1 > numero2) {
-    console.log ("El numero mayor es: " + numero1);
-    alert ("El numero mayor es " + numero1)
-}else {
-    console.log("El numero mayor es: " + numero2)
-    alert ("El numero mayor es " + numero2)
-}
-
-
-}
-
-
-// 9.algoritmo que determine el menor entre 3 numeros 
-
-function numMenor(){
-
-    alert ("Este algoritmo determina el menor entre tres numeros")
     let numero1 = 0
     let numero2 = 0
-    let numero3 = 0
 
     numero1 = parseFloat(prompt("Ingrese el primer numero"));
     numero2 = parseFloat(prompt("Ingrese el segundo numero"));
-    numero3 = parseFloat(prompt("Ingrese el tercer numero"));
 
-    let menor 
+    if (numero1 === numero2){
+        console.log("El numero es igual" + numero1 === numero2)
+        alert("El numero es igual")
+    }
 
-    if (numero1 < numero2 && numero1 < numero3) {
-        menor = numero1;
-    } else if (numero2 < numero1 && numero2 < numero3){
-        menor = numero2;
-    }else{
-        menor = numero3;
-    }   
-    alert ("El numero menor es " + menor);
+    else if (numero1 > numero2) {
+        console.log ("El numero mayor es: " + numero1);
+        alert ("El numero mayor es " + numero1)
+    }else {
+        console.log("El numero mayor es: " + numero2)
+        alert ("El numero mayor es " + numero2)
+    }
+    
+    
+}
+
+
+//algoritmo que determine el menor entre 3 numeros 
+
+function numMenor(){
+
+        alert ("Este algoritmo determina el menor entre tres numeros")
+        let numero1 = 0
+        let numero2 = 0
+        let numero3 = 0
+
+        numero1 = parseFloat(prompt("Ingrese el primer numero"));
+        numero2 = parseFloat(prompt("Ingrese el segundo numero"));
+        numero3 = parseFloat(prompt("Ingrese el tercer numero"));
+
+        let menor 
+
+        if (numero1 < numero2 && numero1 < numero3) {
+            menor = numero1;
+        } else if (numero2 < numero1 && numero2 < numero3){
+            menor = numero2;
+        }else{
+            menor = numero3;
+        }   
+        alert ("El numero menor es " + menor);
 
 }
 
 
-// 10. el colegio abc requiere un sistema que le permita validad x estudiante si aprovo o reprovo una materia teniendo en cuenta 9 notas del 1-10 y se aprueba con una nota de 6.1
+//el colegio abc requiere un sistema que le permita validad x estudiante si aprovo o reprovo una materia teniendo en cuenta 9 notas del 1-10 y se aprueba con una nota de 6.1
 
 function validarnotascol(){
 let sumanotas = 0; 
 let cantidadnotas = 0;
 for ( let i = 0; i <= 8; i++){
-let nota = parseFloat(prompt("ingrese sus notas" ));
-sumanotas = nota + sumanotas
-no = 0
+    let nota = parseFloat(prompt("ingrese sus notas" ));
+    sumanotas = nota + sumanotas
+    no = 0
 }
 let promedio = sumanotas / 10
 
 if (promedio < 6.1){
-console.log("reprobaste la materia" + promedio);
-alert("reprobaste la materia" + promedio);
+    console.log("reprobaste la materia" + promedio);
+    alert("reprobaste la materia" + promedio);
 } else {
-console.log("aprovo la materia" + promedio);
-alert("aprovo la materia" + promedio);
+    console.log("aprovo la materia" + promedio);
+    alert("reprobaste la materia" + promedio);
 }
 
 }
 
 
-// 11. un indivuduo decide invertir su capital en un banco y requieresaber cuanto dinero ganara despues de n numero de años teniendo en cuenta que el banco paga un interes mensual de 0,7%
+//un indibuduo decide invertir su capital en un banco y requieresaber cuanto dinero ganara despues de n numero de años teniendo en cuenta que el banco paga un interes mensual de 0,7%
 
 function invcapital(){
 const porcentaje = 8.4;
